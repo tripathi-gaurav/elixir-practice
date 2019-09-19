9a7b7a92db652b94c1fdb07e03d80189b4446a92
Gaurav <gaurav@tripathi.org>
2019-09-19 06:59:15 +0000

    added controller for palindrome

--- lib/practice_web/controllers/page_controller.ex
+++ lib/practice_web/controllers/page_controller.ex
@@ -21,6 +21,8 @@ defmodule PracticeWeb.PageController do
     render conn, "factor.html", x: x, y: y
   end
 
-  # TODO: Add an action for palindrome.
+  def palindrome(conn, %{"x" => x}) do
+   x === Practice.palindrome(x)
+   render conn, "palindrome.html", x : x, y : y
   # TODO: Add a template for palindrome over in lib/*_web/templates/page/??.html.eex
 end
